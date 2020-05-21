87dce10cec7738594dd23b0dc60fc0f5cbb466e3
Marcus Riesmeier <marcus.riesmeier@umit.at>
2020-05-21 21:00:44 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+Creates a pdf-notebook/journal out of your journal/xournal/xournalpp/... notes, using latex + python.
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+description = ("Creates a pdf-notebook/journal out of your journal/xournal/xournalpp/... notes, using latex + python.")
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="journalmk",
+    version="2020.1a",
+    author="Marcus Riesmeier",
+    author_email="marcus.riesmeier@umit.com",
+    license="BSD 3-Clause License",
+    description=description,
+    long_description=description,
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ),
+)
+
