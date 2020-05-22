c2c828367e01b57fbf68d4a0f9d11dff9900de7c
Marcus Riesmeier <marcus.riesmeier@umit.at>
2020-05-22 02:01:12 +0200

    Collect minimal feature set for latex document

--- journalmk/journalmk.py
+++ journalmk/journalmk.py
@@ -1,4 +1,4 @@
-import os, json, hashlib, subprocess
+import os, json, hashlib, subprocess, datetime, pathlib
 
 metadata_fname = "journalmk.json"
 
@@ -78,6 +78,23 @@ def make_pdf_notes(note_dirs, notes_ending, pdf_command):
                 subprocess.run(command)
 
 
+def parse_timestamps(note_dirs):
+    for note_dir in note_dirs:
+        timestamps = list()
+        for note in note_dirs[note_dir]["notes"]:
+            stem = pathlib.Path(note).stem
+            # https://strftime.org/
+            timestamps.append(datetime.datetime.strptime(stem, "%Y-%m-%d-Note-%H-%M"))
+
+        note_dirs[note_dir].update(timestamps=timestamps)
+
+    return note_dirs
+
+
+def make_chronological_journal(note_dirs):
+    pass
+
+
 with open("journalmkrc.json") as conf_file:
     conf = json.load(conf_file)
 
@@ -85,11 +102,11 @@ note_dirs = find_directories(conf["root"], conf["notes_directory_name"])
 note_dirs = parse_metadata(note_dirs)
 note_dirs = find_notes(note_dirs, conf["notes_file_ending"])
 make_pdf_notes(note_dirs, conf["notes_file_ending"], conf["notes_pdf_export_command"])
-# note_dirs = parse_timestamps(note_dirs)
+note_dirs = parse_timestamps(note_dirs)
+print(note_dirs)
 
 if conf["journal_type"] == "chronological":
-    pass
-    # make_chronological_journal(note_dirs)
+    make_chronological_journal(note_dirs)
 
 elif conf["journal_type"] == "tree":
     raise NotImplementedError
